395c16d65f8a20eba181c25940d535564354dfbd
Thitiwut Pattanasuttinont <93137058+aFluffyHotdog@users.noreply.github.com>
2026-01-17 11:18:59 -0500

    added PlayerMovement

---
+++ My project/Assets/MoveCamera.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class MoveCamera : MonoBehaviour
+{
+    public Transform cameraPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
