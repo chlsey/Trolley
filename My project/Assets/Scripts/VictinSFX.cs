3061bd2c370e4da877a93616c95bd09c638266b5
cjpcpc11 <cpollack489@gmail.com>
2026-01-28 17:35:45 -0500

    KillCounter
    Killcount logic added
    Killcount display added

--- My project/Assets/Scripts/VictinSFX.cs
+++ My project/Assets/Scripts/VictinSFX.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
+using TMPro;
 
 public class VictinSFX : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip splat;
     public LayerMask layerMask;
+    public static int killCount = 0;
+    private bool triggered = false;
+    public TextMeshPro text;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        text.text = "KILLS: 0";
     }
     private void OnTriggerEnter(Collider other)
     {
-        if ((layerMask.value & (1 << other.gameObject.layer)) > 0)
+        if (triggered) return;
+
+        if ((layerMask.value & (1 << other.gameObject.layer)) > 0 )
         {
+            triggered = true;
             audioSource.PlayOneShot(splat);
+            killCount ++;
+            text.text = "KILLS: " + killCount;
+            Debug.Log(killCount);
         }
     }
 
