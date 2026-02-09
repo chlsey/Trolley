d94ea8e5a6d74662551b2e3c3db07cc88bb95cd4
akcljt <139833916+akcljt@users.noreply.github.com>
2026-02-09 17:11:46 -0500

    finished rating meter!

--- My project/Assets/Scripts/lever.cs
+++ My project/Assets/Scripts/lever.cs
@@ -11,6 +11,8 @@ public class lever : MonoBehaviour
     public Animator armAnim;
     public AudioSource audioSource;
     public AudioClip switchSound;
+    public Rating rating;
+    public bool leverFlipped = false;
 
     private bool nearLever;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,6 +33,8 @@ public class lever : MonoBehaviour
             armAnim.SetTrigger("Switch");
             audioSource.PlayOneShot(switchSound);
             Debug.Log("TrackSwitched");
+            rating.ChangeRating(-0.5f);
+            leverFlipped = true;
             enabled = false;
         }
     }
@@ -46,17 +50,19 @@ public class lever : MonoBehaviour
 
     void Awake()
     {
-        trolleyMovement ??= FindObjectOfType<TrolleyMovement>();
+        trolleyMovement ??= FindFirstObjectByType<TrolleyMovement>();
         if (redGreenLight == null)
-            redGreenLight = FindObjectOfType<RedGreenLight>();
+            redGreenLight = FindFirstObjectByType<RedGreenLight>();
         if (greenRedLight == null)
-            greenRedLight = FindObjectOfType<GreenRedLight>();
+            greenRedLight = FindFirstObjectByType<GreenRedLight>();
         if (anim == null)
             anim = GetComponent<Animator>();
         if (armAnim == null)
             armAnim = GetComponentInChildren<Animator>();
         if (audioSource == null)
             audioSource = GetComponent<AudioSource>();
+        if (rating == null)
+            rating = FindFirstObjectByType<Rating>();
     }
     
 }
