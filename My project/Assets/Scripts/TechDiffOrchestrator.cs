65b014831203e78b105dd8105597a72897a00df5
chlsey <chelseywang0219@gmail.com>
2026-02-22 14:43:22 -0500

    added solid background to the menu page

---
+++ My project/Assets/Scripts/TechDiffOrchestrator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TechDiffOrchestrator : MonoBehaviour
+{
+    [Header("VO Clips")]
+    public AudioClip techDiffIntroClip;  
+    public AudioClip lightsOn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
