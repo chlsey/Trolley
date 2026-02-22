ea2a220529f6997d4ba210416daf4af7b8aa0ab6
Thitiwut Pattanasuttinont <93137058+aFluffyHotdog@users.noreply.github.com>
2026-02-22 10:40:29 -0500

    Level 4: Endings Programmed

--- My project/Assets/Scripts/Level4/LevelFour.cs
+++ My project/Assets/Scripts/Level4/LevelFour.cs
@@ -5,23 +5,52 @@ public class LevelFourIntro : MonoBehaviour
 {
     [Header("VO Clips")]
     public AudioClip levelFourIntroClip;  
+    public AudioClip levelFourEndingOne;
+    public AudioClip lightsOn;
 
+    public EndTrigger endTrigger;
     public TrolleyMovement trolleyMovement;
-    public CurtainController curtainController;
+    // public CurtainController curtainController;
+    public Lever lever;
+    public ClockBehavior clock;
 
-    public Light trackOneLight;
-    public Light trackTwoLight;
+    // public Light trackOneLight;
+    // public Light trackTwoLight;
     public Light jimmyLight;
     public GameObject jimmy;
+    
+    private bool endingStarted;
+
     void Start()
     {
+        jimmyLight.intensity = 0f;
+        endingStarted = false;
+        clock.rotate = false;
         trolleyMovement.followSpline = false;
         StartCoroutine(PlayLevelFourIntroSeq());
     }
 
+    void Update()
+    {
+        if (lever.leverFlipped == true && endingStarted == false)
+        {
+            Debug.Log("lvl4 ending 1");
+            StopAllCoroutines();
+            StartCoroutine(PlayLevelFourEndingOne());
+            endingStarted = true;   
+        }
+    }
+
+    private IEnumerator PlayLevelFourEndingOne()
+    {
+        VOManager.Instance.PlayLine(levelFourEndingOne);
+        yield return new WaitForSeconds(16);
+        endTrigger.TriggerEnd();
+        
+    }
     private IEnumerator PlayLevelFourIntroSeq()
     {
-        yield return CurtainController.Instance.CloseCurtains();
+        // yield return CurtainController.Instance.CloseCurtains();
         VOManager.Instance.PlayLine(levelFourIntroClip);
         Debug.Log("lvl 4 intro played!");
         
@@ -29,21 +58,19 @@ public class LevelFourIntro : MonoBehaviour
         // Show volunteer on screen
 
         // Show jimmy waving (at 15 seconds)
+        yield return new WaitForSeconds(2);
+        VOManager.Instance.PlaySoundFX(lightsOn);
 
         // Gasp, lights turn red "Oh no where's the second track" (at 23 sec)
+        yield return new WaitForSeconds(8);
 
         // "That's right! your lever will push jimmy onto the tracks" (at 29 sec)
+        // yield return new WaitForSeconds(6);
 
         // Show the Terms & Condition on screen (at 43 sec)
+        // yield return new WaitForSeconds(14);
         trolleyMovement.followSpline = true;
-        yield return curtainController.OpenCurtains();
-
-        
-
-
-
-
-
-
+        clock.rotate = true;
+        // yield return curtainController.OpenCurtains();
     }
 }
---
+++ My project/Assets/Scripts/Level4/LevelFourTrackB.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class LevelFourChoiceB : MonoBehaviour
+{
+    public EndTrigger endTrigger;
+    public Lever lever;
+    public AudioClip levelFourEndingTwo;
+    void OnTriggerEnter(Collider other)
+    {
+        if(lever.leverFlipped == false)
+        {
+            StopAllCoroutines();
+            Debug.Log("lvl 4 track b ending!");
+            StartCoroutine(EndSequence());
+        }
+    }
+
+    private IEnumerator EndSequence()
+    {
+        VOManager.Instance.PlayLine(levelFourEndingTwo);
+        yield return new WaitForSeconds(15f);  
+        endTrigger.TriggerEnd(); 
+
+    }
+}
