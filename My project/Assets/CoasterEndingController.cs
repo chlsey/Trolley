7e40be84bf7b951aa4f3146a41225926db58765c
chlsey <chelseywang0219@gmail.com>
2026-04-04 02:32:59 -0400

    added escape hallway lines

--- My project/Assets/CoasterEndingController.cs
+++ My project/Assets/CoasterEndingController.cs
@@ -1,13 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class CoasterEndingController : MonoBehaviour
 {
+    public AudioClip intro;
+    public AudioClip nauseaClip;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(CoasterCoroutine());
     }
+
+    private IEnumerator CoasterCoroutine()
+    {
+        if (VOManager.Instance != null)
+        {
+            VOManager.Instance.PlayLine(intro);
+            VOManager.Instance.ShowSubtitle(new List<VOManager.SubtitleLine>
+            {
+                // new VOManager.SubtitleLine("Apologies for the sudden change in... dimentionality", 0f, 5000f)
+            });
+
+            yield return new WaitForSeconds(intro.length);
+
+            // VOManager.Instance.PlayLine(fadeout);
+
+        }
+    }
+
+    private IEnumerator RidingCoroutine()
+    {
+        if (VOManager.Instance != null)
+        {
+            VOManager.Instance.PlayLine(nauseaClip);
+            VOManager.Instance.ShowSubtitle(new List<VOManager.SubtitleLine>
+            {
+                // new VOManager.SubtitleLine("Apologies for the sudden change in... dimentionality", 0f, 5000f)
+            });
+
+            yield return new WaitForSeconds(nauseaClip.length);
+
+            // VOManager.Instance.PlayLine(fadeout);
+
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
